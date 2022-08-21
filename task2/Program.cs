1a9965d0f68c312cd15d67e909b24828cf709997
Alex <newalexis777@gmail.com>
2022-08-21 21:22:01 +0400

    task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -6,35 +6,39 @@ Console.WriteLine("Введите количество вершин: ");
 int n = int.Parse(Console.ReadLine());
 double[] arrayX = new double[n];
 double[] arrayY = new double[n];
-
 int ver = 1;
-double k = 0;
-int i = 0;
-double b = 0;
 
-for (i = 0; i < n; i++)
+for (int i = 0; i < n; i++)
 {
     Console.WriteLine("Введите значение вершинны x" + ver + ":");
     arrayX[i] = double.Parse(Console.ReadLine());
     Console.WriteLine("Введите значение вершинны y" + ver + ":");
     arrayY[i] = double.Parse(Console.ReadLine());
 
-    
     ver++;
 }
 
-k = (arrayY[n - 1] - arrayY[0]) / (arrayX[n - 1] - arrayX[0]);
-b = arrayY[n - 2] - (k * arrayX[n - 2]);
-Console.WriteLine("k == " + k + " b == " + b + " линейной функции ограниченной двумя вершинами " + arrayX[n - 2] + " " + arrayY[n - 2] + "," + arrayX[n - 1] + " " + arrayY[n - 1]);
-
-for (i = 0; i < n; i++)
+void Mastab(double[] array1, double[] array2)
 {
-    k = (arrayY[i] - arrayY[i + 1]) / (arrayX[i] - arrayX[i + 1]);
-    b = arrayY[i] - (k * arrayX[i]);
-    Console.WriteLine("k == " + k + " b == " + b + " линейной функции ограниченной двумя вершинами " + arrayX[i] + " " + arrayY[i] + "," + arrayX[i + 1] + " " + arrayY[i + 1]); 
+Console.WriteLine("Введите K: ");
+double k = double.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+     Console.Write("(" + arrayX[i] + ", " + arrayY[i] + ") " );
 }
 
+Console.WriteLine();
 
+for (int i = 0; i < n; i++)
+{
+    double x = arrayX[i] * k;   
+    double y = arrayY[i] * k;
+    Console.Write("(" + x + ", " + y + ") " );
+}
+}
+
+Mastab(arrayX, arrayY);
 
 
 
