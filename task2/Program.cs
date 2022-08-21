75f58c4cc69861c105dc24fc4274d94c5186bb32
Alex <newalexis777@gmail.com>
2022-08-21 16:53:42 +0400

    task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -4,89 +4,39 @@
 
 Console.WriteLine("Введите количество вершин: ");
 int n = int.Parse(Console.ReadLine());
-int[] arrayX = new int[n];
-int[] arrayY = new int[n];
+double[] arrayX = new double[n];
+double[] arrayY = new double[n];
 
 int ver = 1;
-int k = 0;
+double k = 0;
 int i = 0;
-int maxX = 0;
-int minX = 0;
-int maxY = 0;
-int minY = 0;
-int x = 0;
-int y = 0;
-int b = 0;
-
-//задаем вершины
+double b = 0;
 
 for (i = 0; i < n; i++)
 {
-    Console.WriteLine("Введите вершину " + ver);
     Console.WriteLine("Введите значение вершинны x" + ver + ":");
-    arrayX[i] = int.Parse(Console.ReadLine());
+    arrayX[i] = double.Parse(Console.ReadLine());
     Console.WriteLine("Введите значение вершинны y" + ver + ":");
-    arrayY[i] = int.Parse(Console.ReadLine());
+    arrayY[i] = double.Parse(Console.ReadLine());
 
     
     ver++;
 }
 
-//находим k и b
+k = (arrayY[n - 1] - arrayY[0]) / (arrayX[n - 1] - arrayX[0]);
+b = arrayY[n - 2] - (k * arrayX[n - 2]);
+Console.WriteLine("k == " + k + " b == " + b + " линейной функции ограниченной двумя вершинами " + arrayX[n - 2] + " " + arrayY[n - 2] + "," + arrayX[n - 1] + " " + arrayY[n - 1]);
+
 for (i = 0; i < n; i++)
 {
     k = (arrayY[i] - arrayY[i + 1]) / (arrayX[i] - arrayX[i + 1]);
     b = arrayY[i] - (k * arrayX[i]);
-
-    if (arrayX[i] > arrayX[i+1])
-    {
-        maxX = arrayX[i];
-    }
-    else
-    {
-        maxX = arrayX[i+1];
-    }
-
-    if (arrayY[i] > arrayY[i+1])
-    {
-        maxY = arrayY[i];
-    }
-    else
-    {
-        maxY = arrayY[i+1];
-    }
-
-    if (maxX > maxY)
-    {
-        Console.SetCursorPosition(minX, minY);
-        Console.WriteLine("*");
-
-        while(minX <= maxX)
-        {
-            x = arrayX[i] + 1;
-            y = k * x + b;
-            Console.SetCursorPosition(x, y);
-            Console.WriteLine("*");
-            minX++;
-        }
-
+    Console.WriteLine("k == " + k + " b == " + b + " линейной функции ограниченной двумя вершинами " + arrayX[i] + " " + arrayY[i] + "," + arrayX[i + 1] + " " + arrayY[i + 1]); 
 }
-    else
-    {
-        Console.SetCursorPosition(minX, minY);
-        Console.WriteLine("*");
 
-        while(minY <= maxY)
-        {
-            y = arrayY[i] + 1;
-            x = (y - b) / k;
-            Console.SetCursorPosition(x, y);
-            Console.WriteLine("*");
-            minY++;
-        }
 
-    }
-}
+
+
 
 
 
